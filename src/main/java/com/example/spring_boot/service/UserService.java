fb589c627f33a2f708a3f883ac45525cb800b6e2
eliseevps <eliseev.ps@mail.ru>
2023-07-29 22:06:55 +0500

    App migration CRUD on Spring Boot

---
+++ src/main/java/com/example/spring_boot/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot.service;
+
+import com.example.spring_boot.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+
+    void save(User user);
+    User getUser(Long id);
+    void delete(Long id);
+}
